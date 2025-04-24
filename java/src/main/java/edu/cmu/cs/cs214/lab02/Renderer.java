af8c4e4cd65c0c248d61df94cc8b7ba5ffb280a8
Chagdarsuren Enkhbayar <chagdarsurenenkhbayar@Chagdarsurens-MacBook-Air.local>
2025-04-25 02:22:47 +0800

    Zasaw

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -1,19 +1,29 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.*;
+import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 
 public class Renderer {
-    public Rectangle rectangle;
+    private Rectangle rectangle;
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
     
     Renderer(Rectangle rectangle) {
         this.rectangle = rectangle;
     }
 
     void draw() {
-        double area = rectangle.getArea();
+        java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Renderer.class.getName());
+        double area = rectangle.getArea(); // Added this line to calculate the area of the rectangle
+        String message = """
+                Shape printed
+                Its area is %f
+                """.formatted(area); // Fixed the undefined variable 'area' by defining it above
+        logger.info(message);
 
         // assume implementation
 
-        System.out.println("Shape printed\n" + "Its area is " + area);
+        System.out.println("Shape printed\n" + "Its area is " + area); // Fixed the undefined variable 'area' by defining it above
     }
 }
