38069e9b6ac54463523b57d7340275d59252185b
Chagdarsuren Enkhbayar <chagdarsurenenkhbayar@Chagdarsurens-MacBook-Air.local>
2025-04-25 02:23:47 +0800

    Zasagdaw

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,17 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.rendering.Renderer; // Assuming Renderer is in this package
 
 public class Main {
     public static void main(String[] args) {
+        // Create a rectangle with width 2 and height 3
         Rectangle rectangle = new Rectangle(2, 3);
 
+        // Create a renderer object to render the rectangle
         Renderer renderer = new Renderer(rectangle);
 
+        // Draw the rectangle using the renderer
         renderer.draw();
     }
 }
